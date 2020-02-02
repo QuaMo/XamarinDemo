2140e155cc96f1c47a9efe05df64ed4da3491b89
Mohamed El Kachari <Mohamed.elkachari@brightest.be>
2020-02-02 23:17:20 +0100

    Add ViewModel

---
+++ App1/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+                TheNote = string.Empty;
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => TheNote;
+            set
+            {
+                theNote = value:
+
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        Command SaveCommand { get; }
+        Command EraseCommand { get; }
+
+
+    }
+}
